2c882828017ccdb936538320063a9cc2bb5f021e
ThomasTonho <antonellithomasv@gmail.com>
2026-01-29 23:18:08 -0300

    Subindo Serializers

--- api/models.py
+++ api/models.py
@@ -1,16 +1,11 @@
 from django.db import models
 
+
 class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=120)
     telefone = models.CharField(max_length=20)
     tipo = models.CharField(max_length=20)
-
-class Pagamento(models.Model):
-    data_pagamento = models.DateField()
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20)
-    contrato = models.ForeignKey('Contrato', on_delete=models.CASCADE)
 
 
 class Imovel(models.Model):
@@ -20,6 +15,7 @@ class Imovel(models.Model):
     status = models.CharField(max_length=20)
     locador = models.ForeignKey(Usuario, related_name='imoveis', on_delete=models.CASCADE)
 
+
 class Contrato(models.Model):
     data_inicio = models.DateField()
     data_fim = models.DateField()
@@ -27,3 +23,10 @@ class Contrato(models.Model):
     imovel = models.ForeignKey(Imovel, on_delete=models.CASCADE)
     locador = models.ForeignKey(Usuario, related_name='contratos_locador', on_delete=models.CASCADE)
     locatario = models.ForeignKey(Usuario, related_name='contratos_locatario', on_delete=models.CASCADE)
+
+
+class Pagamento(models.Model):
+    data_pagamento = models.DateField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20)
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
