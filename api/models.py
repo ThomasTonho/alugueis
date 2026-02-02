92e302dbb63249231a056772a38482fac3dd2d01
ThomasTonho <antonellithomasv@gmail.com>
2026-02-02 09:50:35 -0300

    Atualizando models

--- api/models.py
+++ api/models.py
@@ -1,32 +1,50 @@
 from django.db import models
 
-
 class Usuario(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.CharField(max_length=120)
-    telefone = models.CharField(max_length=20)
-    tipo = models.CharField(max_length=20)
+    TIPO_CHOICES = [
+        ('LOCADOR', 'Locador'),
+        ('LOCATARIO', 'Locatário')
+    ]
 
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    tipo = models.CharField(choices=TIPO_CHOICES)
+
+    def __str__(self):
+        return self.nome
 
 class Imovel(models.Model):
-    titulo = models.CharField(max_length=100)
-    tipo = models.CharField(max_length=50)
-    valor_aluguel = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20)
-    locador = models.ForeignKey(Usuario, related_name='imoveis', on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ('DISPONIVEL', 'Disponível'),
+        ('ALUGADO', 'Alugado')
+    ]
 
+    titulo = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    valor_aluguel = models.DecimalField(max_digits=11, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    locador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='imoveis')
+
+    def __str__(self):
+        return self.titulo
 
 class Contrato(models.Model):
     data_inicio = models.DateField()
     data_fim = models.DateField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    imovel = models.ForeignKey(Imovel, on_delete=models.CASCADE)
-    locador = models.ForeignKey(Usuario, related_name='contratos_locador', on_delete=models.CASCADE)
-    locatario = models.ForeignKey(Usuario, related_name='contratos_locatario', on_delete=models.CASCADE)
+    imovel = models.ForeignKey(Imovel, on_delete=models.CASCADE, related_name='contratos')
+    locador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='locador')
+    locatario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='locatario')
 
+    def __str__(self):
+        return f"Contrato {self.id}"
 
 class Pagamento(models.Model):
     data_pagamento = models.DateField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20)
-    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+    status = models.BooleanField()
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE, related_name='pagamentos')
+
+    def __str__(self):
+        return f"Pagamento {self.id}"
